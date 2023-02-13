4196cae669b563328e31b31ca2a709d06fd3a575
Dinesh Kumar <119421483+dineshkumar-stack@users.noreply.github.com>
2023-02-13 12:37:23 +0530

    Added callback hell

---
+++ script.js
@@ -0,0 +1,40 @@
+let india = document.createElement("div");
+document.body.append(india);
+
+
+setTimeout(()=>{
+    india.innerHTML = 10
+      setTimeout(()=>{
+        india.innerHTML = 9
+        setTimeout(()=>{
+          india.innerHTML = 8
+          setTimeout(()=>{
+            india.innerHTML = 7
+            setTimeout(()=>{
+              india.innerHTML = 6
+              setTimeout(()=>{
+                india.innerHTML = 5
+                setTimeout(()=>{
+                  india.innerHTML = 4
+                  setTimeout(()=>{
+                    india.innerHTML = 3
+                    setTimeout(()=>{
+                      india.innerHTML = 2
+                      setTimeout(()=>{
+                        india.innerHTML = 1
+                        setTimeout(()=>{
+                          india.innerHTML = "Happy Independence Day"
+                      },1000)  
+                    },1000)  
+                  },1000)  
+                },1000)  
+              },1000)  
+            },1000)  
+          },1000)  
+        },1000)  
+      },1000)  
+    },1000)  
+  },1000)  
+
+
+
